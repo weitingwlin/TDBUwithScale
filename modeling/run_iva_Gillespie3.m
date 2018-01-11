8c5bb08bef01bdf4df703c96ec3288d501d25976
weitingwlin <weiting.lin.lin@gmail.com>
2018-01-10 17:29:16 -0800

    add analysis ladybug decline

--- modeling/run_iva_Gillespie3.m
+++ modeling/run_iva_Gillespie3.m
@@ -1,6 +1,7 @@
 
     clear; clc
 
+
 %% calculate stochastic parameters   
     tlim = 100; % total time of simulation    
     trec = 10;  % time steps recorded 
@@ -8,18 +9,24 @@
    initL = 0.1; % initial fraction of Ladybug
    initA = 0.7; % initial fraction of Aphids
 %% Set model scenario & load parameters
-    scen = 1;
+    scen = 2;
     script_Gillespie3_parameters
     % overwrite default parameters
-    exitP = 0.1;
-    parameters.exitP = exitP;
-    tlim = 20; % allows the ladybeetle populations to decrease with time
-    ts = tlim-trec +1: 1: tlim; % time points recorded
+    %exitP = 0.05;
+    %parameters.exitP = exitP;
+   % mH = 0.15;
+    % parameters.mH = mH;% Mortality
+  sP = 5;
+  parameters.sP = sP;
+  %  tlim = 30; % allows the ladybeetle populations to decrease with time
+ % 
     it = 10;
     initL = 0.3;
-    description = ['Scien 1', para2str(exitP,tlim,initL)]
+    description = [para2str(scen), para2str( sP, tlim, initL)]
+ % description = 'Scien 1'
 %%
     simA = []; simL = []; simD =[];
+    ts = tlim-trec +1: 1: tlim; % time points recorded
     rng(1); % for reproducibility
     tic
     parfor i = 1:it
@@ -48,6 +55,7 @@ figure
 mysubplot(3,1,0, description)
 simD_corrected = simD + repelem([0:11:(it-1)*11], trec);
 % plot(1:length(simD), simD_corrected, 'o')
+
 [Hax,H1,H2] =TDBU_trend_plot(simA, simL, simD_corrected);
 %% Calculate R_TD, R_BU and simulate null model
 sh = 1000;%10000;
@@ -58,6 +66,11 @@ toc
 figure
 mysubplot(3,1,0, description)
 TDBU_plot(out)
+%% 
+figure
+mysubplot(3,1,0, description)
+
+TDBU_diff_plot(out)
 %%
 % R_TD (for 1-, 3-, 9-, 27 plant scale)
     out.real(1,:)
